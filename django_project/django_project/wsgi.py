90e65f9cdb4b827a5e0feb350813345116ebb07f
Amber Bhanarkar <41240819+amberbhanarkar@users.noreply.github.com>
2019-05-16 17:00:44 +0530

    Update wsgi.py

--- django_project/django_project/wsgi.py
+++ django_project/django_project/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append("/home/TSF/pay-gate/django_project")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
